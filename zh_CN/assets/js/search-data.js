50b071730b9685bdca569a4c7f613cf057fcf4a6
ShawnLess <shawnless.xie@gmail.com>
2025-12-26 19:46:01 +0000

    Deploying to gh-pages from @ ShawnLess/JoeyXie@11415f8e0e2aaced1ff27b3d791dd500333fe3a3 🚀

--- zh_CN/assets/js/search-data.js
+++ zh_CN/assets/js/search-data.js
@@ -9,55 +9,29 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+  },{id: "nav-chessground",
+          title: "ChessGround",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/blog/";
           },
-        },{id: "post-modern-c-features",
+        },{id: "nav-chesscourse",
+          title: "ChessCourse",
+          description: "From Pawn to King",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/chesscourse/";
+          },
+        },{id: "post-test",
       
-        title: "Modern C++ features",
+        title: "Test",
       
-      description: "Brief notes of modern C++ features",
+      description: "",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/2025/06/11/Modern-C++-Features.html";
-        
-      },
-    },{id: "post-addressing-of-gpgpu",
-      
-        title: "Addressing of GPGPU",
-      
-      description: "Addressing scheme of GPU.",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/2025/03/24/Addressing-of-GPU-Scaling-Out-Up.html";
-        
-      },
-    },{id: "post-discriminative-learning-vs-generative-learning",
-      
-        title: "Discriminative learning vs Generative learning",
-      
-      description: "A concrete comparison of the discriminative and generative learning.",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/2025/02/21/Discriminative-Generative.html";
-        
-      },
-    },{id: "post-fundamental-math-for-machine-learning",
-      
-        title: "Fundamental Math for Machine Learning",
-      
-      description: "A learning notes of the basic math knowledge and conceptions for machine learning.",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/2025/01/27/Fundamental-Math-for-Machine-Learning.html";
+          window.location.href = "/2025/12/25/test.html";
         
       },
     },{id: "news-moved-to-united-state-learned-how-to-play-chess",
@@ -78,27 +52,7 @@ ninja.data = [{
           section: "News",},{id: "news-全家重新搬到美国",
           title: '全家重新搬到美国',
           description: "",
-          section: "News",},{id: "projects-mapu",
-          title: 'MaPU',
-          description: "A Mathematical Computing Architecture with VLIW and CGRA features.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_mapu/";
-            },},{id: "projects-celerity",
-          title: 'Celerity',
-          description: "Open-Source RISC-V Tiered Accelerator Fabric SoC.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_celerity/";
-            },},{id: "projects-sdh",
-          title: 'SDH',
-          description: "Software Defined Hardware.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_SDH/";
-            },},{id: "projects-yitian710",
-          title: 'YiTian710',
-          description: "Alibaba’s Yitian 710 Is China’s First Homegrown Cloud-Native CPU to Be Put Into Large-Scale Use,",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_YiTian710/";
-            },},{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
